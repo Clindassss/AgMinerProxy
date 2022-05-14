2ae486c4482b296342baf080e6670282a2cacbb3
Clindassss <ag2m195@gmail.com>
2022-05-14 13:09:41 +0800

    删除老版本程序

--- linux-install.sh
+++ linux-install.sh
@@ -22,15 +22,6 @@ PATH_TURN_ON="/etc/profile.d"
 PATH_TURN_ON_SH="/etc/profile.d/AG.sh"
 
 ISSUE() {
-    echo "1.1.2.1"
-    echo "1.1.5"
-    echo "2.0.1"
-    echo "2.1.0"
-    echo "2.1.1"
-    echo "2.2.0"
-    echo "2.2.1"
-    echo "2.2.2"
-	echo "2.2.3"
 	echo "2.2.4"
 }
 
